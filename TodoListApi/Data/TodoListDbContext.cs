b608ad3039d34dbb97cb47a2391c1e057e30b7eb
tri.le <leduytri92@gmail.com>
2023-12-30 00:26:57 +0700

    Add TodoList.Api project

---
+++ TodoListApi/Data/TodoListDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoList.Api.Data
+{
+    public class TodoListDbContext: DbContext
+    {
+
+        public TodoListDbContext(DbContextOptions<TodoListDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ TodoListApi/Entities/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿using TodoList.Api.Enums;
+
+namespace TodoListApi.Entities
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid? Assignee { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public Priority Priority { get; set; }
+
+        public Status Status { get; set; }  
+    }
+}
