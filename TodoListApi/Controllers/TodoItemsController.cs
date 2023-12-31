2bbad751286344e207fa62f31f2dcfa40372e460
tri.le <leduytri92@gmail.com>
2023-12-31 12:41:22 +0700

    Create CRUD APIs for Todo list

--- TodoListApi/Controllers/TodoItemsController.cs
+++ TodoListApi/Controllers/TodoItemsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TodoList.Api.Entities;
 using TodoList.Api.Repositories;
 
 namespace TodoList.Api.Controllers
@@ -19,6 +20,49 @@ namespace TodoList.Api.Controllers
         {
             var todoItems = await _todoRepository.GetAll();
             return Ok(todoItems);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TodoItem todoItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var todoItems = await _todoRepository.Create(todoItem);
+            return CreatedAtAction(nameof(GetById), new { id = todoItem.Id }, todoItems);
+        }
+
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update(Guid id, TodoItem todoItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todoItemDb = await _todoRepository.GetById(id);
+            if (todoItemDb == null)
+            {
+                return NotFound($"Find by {id} is not found");
+            }
+
+            var todoItems = await _todoRepository.Update(todoItem);
+            return Ok(todoItems);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var todoItem = await _todoRepository.GetById(id);
+            if (todoItem == null)
+            {
+                return NotFound($"Find by {id} is not found");
+            }
+            return Ok(todoItem);
         }
     }
 }
