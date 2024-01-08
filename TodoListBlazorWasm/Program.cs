c10071c87805bed2111931c95d8c283146266245
tri.le <leduytri92@gmail.com>
2024-01-09 01:14:24 +0700

    Handle form login and manage state

--- TodoListBlazorWasm/Program.cs
+++ TodoListBlazorWasm/Program.cs
@@ -1,17 +1,24 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TodoListBlazorWasm;
 using TodoListBlazorWasm.Services;
+using TodoListBlazorWasm.Utilities;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazorBootstrap();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
 
 builder.Services.AddTransient<ITodoItemApiClient, TodoItemApiClient>();
 builder.Services.AddTransient<IUserApiClient, UserApiClient>();
 
+builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped(sp => new HttpClient
 { 
     BaseAddress = new Uri(builder.Configuration["BackendApiUrl"]!)
